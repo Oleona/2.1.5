4734f0757986055273dc72c4c329ab92206016d7
Oleona <Ole-onna@mail.ru>
2023-01-28 18:19:41 +0300

    Initial commit

---
+++ PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class Egg6 {
+
+    private final Needle7 needle;
+
+    @Autowired
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + needle.toString();
+    }
+}
